74b3bca2517f6c13fe4b30ffb7d6dbe40a0f017d
CYAN4S <37798132+CYAN4S@users.noreply.github.com>
2020-08-25 19:09:34 +0900

    Updates

---
+++ precache-manifest.88a7f893e3a9bab301a2351ac9e0c465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca662eb44143907a8943ac0d775e8920",
+    "url": "/moview/index.html"
+  },
+  {
+    "revision": "5a5025df97d1fc262a6d",
+    "url": "/moview/static/css/main.ca4fbc71.chunk.css"
+  },
+  {
+    "revision": "df4d8f92d32ce10ce960",
+    "url": "/moview/static/js/2.3c27e900.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/moview/static/js/2.3c27e900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a5025df97d1fc262a6d",
+    "url": "/moview/static/js/main.8707a22e.chunk.js"
+  },
+  {
+    "revision": "20756ed7de00415e2a4d",
+    "url": "/moview/static/js/runtime-main.c0004787.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2020/precache-manifest.77904ae96130d43a1039fbce499dbf29.js"
+  "/moview/precache-manifest.88a7f893e3a9bab301a2351ac9e0c465.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie_app_2020/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moview/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
